71da22918b9d67785327f4cbc41fcd1771a75b19
MaqhobelaKao <kaomaqhobela@gmail.com>
2024-06-13 15:16:27 +0200

    Update and delete and edit todo application

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -5,11 +5,21 @@ namespace App\Livewire;
 use App\Models\Todo;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+
     #[Rule('required|min:5|max:50|string')]
     public $name;
+
+    public $search;
+
+    public $editingId;
+
+    #[Rule('required|min:5|max:50|string')]
+    public $editingName;
 
     public function createTodo(){
         // validation
@@ -23,11 +33,45 @@ class TodoList extends Component
 
         // send the the feedback
         session()->flash('message','Create '.$validated['name'].' todo');
+
+        // reseting a page
+        $this->resetPage();
     }
 
-    public function render()
+    public function toggleCompleted($todoId){
+       $todo = Todo::findOrFail($todoId);
+       $todo->completed = !$todo->completed;
+       $todo->save();
+    }
+
+    public function editTodo($todoId){
+        $this->editingId = $todoId;
+        $this->editingName = Todo::findOrFail($todoId)->name;
+    }
+
+    public function cancelEdit(){
+        $this->reset('editingId', 'editingName');
+    }
+
+
+    /**
+     * @return [type]
+     */
+    public function updateTodo(){
+        $this->validateOnly('editingName');
+        $todo = Todo::findOrFail($this->editingId);
+        $todo->name = $this->editingName;
+        $todo->save();
+        $this->cancelEdit();
+    }
+
+    public function deleteTodo($todoId) {
+        Todo::find($todoId)->delete();
+    }
+
+        public function render()
     {
-        $todos = Todo::all();
+        $todos = Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(5);
         return view('livewire.todo-list',[
             'todos' => $todos
         ]);
